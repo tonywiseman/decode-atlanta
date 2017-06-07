6a8cdd701dd9e3c06dd2ac0f96b589ef62f0007b
212320186 <anthony.wiseman@ge.com>
2017-06-07 09:32:01 -0400

    Update security line

--- security/index.js
+++ security/index.js
@@ -4,7 +4,7 @@
     var request = require('request-promise');
     var cheerio = require('cheerio'); // Basically jQuery for node.js
 
-    module.exports = function (context) {
+    exports.handler = function (context, req) {
         var options = {
             method: 'POST',
             uri: 'http://apps.atl.com/Passenger/FlightInfo/Default.aspx',
@@ -14,7 +14,12 @@
         };
 
         request(options).then(function (data) {
-            var security = [];
+            var international = (req && req.query && req.query.international) || false,
+                line,
+                lineCounter = 0,
+                security = [],
+                selection,
+                selectionRating;
 
             data('div#bodySection_wucSecurityWaitTimes_upnWaitTime')
             .children()
@@ -28,8 +33,51 @@
                     location = checkpoint.split(" Checkpoint : ")[0],
                     status = checkpoint.split(" Checkpoint : ")[1];
 
-                security.push({location: location, status: status});
+                if (status.toLowerCase !== "closed") {
+                    security.push({ location: location, status: status });
+                }
             });
+
+            for (lineCounter; lineCounter < security.length; lineCounter += 1) {
+                line = security[lineCounter];
+
+                if (international) {
+                    if (line.location.toLowerCase() === "international") {
+                        selection = line;
+                        break;
+                    }
+                } else {
+                    if (line.location.toLowerCase() !== "international") {
+                        switch (line.status.toLowerCase()) {
+                        case "less than 15 minutes":
+                            selection = line;
+                            selectionRating = 1;
+                            break;
+                        case "15 - 30 minutes":
+                            if (selectionRating > 2) {
+                                selection = line;
+                                selectionRating = 2;
+                            }
+                            break;
+                        case "30 - 45 minutes":
+                            if (selectionRating > 3) {
+                                selection = line;
+                                selectionRating = 3;
+                            }
+                            break;
+                        case "45 - 60 minutes":
+                            if (selectionRating > 4) {
+                                selection = line;
+                                selectionRating = 4;
+                            }
+                            break;
+                        default:
+                            selection = line;
+                            selectionRating = 5;
+                        }
+                    }
+                }
+            }
 
             context.res = { body: JSON.stringify(security, null, 2) };
             context.done();
